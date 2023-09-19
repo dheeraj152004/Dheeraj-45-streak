65a58e590d70501647cff052da2d1a8c08c2eaec
dheeraj152004 <144876088+dheeraj152004@users.noreply.github.com>
2023-09-19 22:24:55 +0530

    Day2.c

---
+++ Day2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch="a";
+  //to check the outcomes of input
+    printf("%d", sizeof(7+4.57));
+    printf("%d",sizeof('A'));
+    printf("%d",sizeof("ch"));
+    printf("%d",sizeof(4.321)+4);
+    printf("%d\n",sizeof(int));
+    printf("This is the outcome\n");
+    printf("Thanks");
+    return 0;
+}
